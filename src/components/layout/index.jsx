f86dc847f4056c2473c7b1a7987f9eed6b95111c
Nicholas Narsing <soren121@sorenstudios.com>
2021-02-21 22:27:03 -0800

    Import font from @fontsource

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,18 +1,11 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import '@fontsource/heebo/400.css';
+import '@fontsource/heebo/600.css';
+import '@fontsource/heebo/900.css';
 
 import './bootstrap.scss';
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Helmet>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;600;900&display=swap" rel="stylesheet" /> 
-            </Helmet>
-            {children}
-        </>
-    )
+    return children;
 };
 
 export default Layout;
