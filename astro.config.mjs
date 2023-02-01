1a9959056e269645e2ce441adcb3cab82e6fcf39
Nicholas Narsing <soren121@sorenstudios.com>
2023-01-31 20:19:56 -0800

    Remove Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig({
     site: 'https://www.sorenstudios.com',
-    integrations: [
-        partytown({
-            // Adds dataLayer.push as a forwarding-event.
-            config: {
-                forward: ["dataLayer.push"],
-            },
-        }),
-    ],
+    integrations: [],
 });
