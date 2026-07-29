59bcb829e58832be29a898f18b7e0d2573defe16
Nicholas Narsing <soren121@sorenstudios.com>
2026-07-29 14:07:34 -0700

    Fix builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,26 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://www.sorenstudios.com',
-    integrations: [],
+  site: 'https://www.sorenstudios.com',
+  integrations: [],
+  build: {
+    inlineStylesheets: 'never'
+  },
+  vite: {
+    ssr: {
+      noExternal: ['bootstrap', 'bootstrap-icons', '@fontsource/heebo']
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'if-function',
+            'import',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+      },
+    },
+  }
 });
